d98bc2db384c93576639c2eeb7a1601e3bc94b26
Evan <obrienem@rose-hulman.edu>
2022-11-28 18:18:24 -0500

    working

--- find.py
+++ find.py
@@ -5,7 +5,7 @@ poly_coef = []
 print('confirm list of coefficients with \'y\'')
 print('cancel list of coefficients with \'n\'')
 while(True):
-	inpt = input(f'input {i}th order polynomial term: ')
+	inpt = input(f'input {i}th order polynomial coefficient: ')
 	if inpt == 'y':
 		break
 	if inpt == 'n':
@@ -20,20 +20,13 @@ x = torch.tensor(float(input('initial value of x: ')), requires_grad=True)
 def forward(x):
 	outp = torch.tensor(0.0)
 	for i in range(len(poly_coef)):
-		outp += poly_coef[i] * (x * 10 ** i)
+		outp += poly_coef[i] * (x ** i)
 	return outp
-
-def critereon(y):
-	return torch.abs(y)
 
 iters = int(input('number of iterations: '))
 for i in range(iters):
 	y = forward(x)
-	print(f'x = {x}, y = {y}')
-	loss = critereon(y)
-	loss.backward()
-	print(x.grad)
-	x.grad.zero_()
-
-print(poly_coef)
-print(x)
+	y.backward()
+	print(f'x = {x}, y = {y}, grad = {x.grad}')
+	delta = float(y / x.grad)
+	x = torch.tensor(float(x-delta), requires_grad=True)
